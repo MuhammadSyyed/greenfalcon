8841d676906919156ecb56bec273eacc5d777dfd
sm_imranabbas <imran.abbas@istoreisend.com>
2025-08-28 14:27:09 +0500

    First Commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import uvicorn
 import os
 
 app = FastAPI()
@@ -31,3 +32,12 @@ def get_catalog():
 @app.get("/catalog.pdf")
 def get_pdf():
     return FileResponse("app/static/catalog.pdf")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",          # Replace "main" with the name of your Python file
+        host="0.0.0.0",    # The IP address to run the server on
+        port=8000,           # The port to run the server on
+        reload=True          # Reloads the server automatically when you change the code
+    )
